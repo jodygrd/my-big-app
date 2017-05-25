bf3c86ca239e18952a739ae4286566dd30015a5a
Jody Lipkin <lipkinjody@gmail.com>
2017-05-24 17:09:35 -0700

    add new group button

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-Category.create!([
-  {name: "Poetry", image: "https://i.imgbox.com/DsQP3SnJ.jpeg"},
-  {name: "Fantasy", image: "https://i.imgbox.com/GU7n9JQm.jpeg"},
-  {name: "Horror", image: "https://i.imgbox.com/bA8gM4Aw.jpg\n"},
-  {name: "Humor", image: "https://i.imgbox.com/fPIqexsE.jpeg"},
-  {name: "Literary Fiction", image: "https://i.imgbox.com/ejkGXRSl.jpg"},
-  {name: "Science Fiction", image: "https://i.imgbox.com/Iy71DSpt.jpeg\n"},
-  {name: "Romance", image: "https://i.imgbox.com/xkS7zeyi.jpg\n"},
-  {name: "Children and YA", image: "https://i.imgbox.com/n49wqpcj.jpeg\n"},
-  {name: "Scriptwriting", image: "https://i.imgbox.com/CqQrByg4.jpeg"},
-  {name: "Nonfiction", image: "https://i.imgbox.com/vmuLzxQ2.jpeg"},
-  {name: "Mystery", image: "https://i.imgbox.com/ogarDhmW.jpeg"},
-  {name: "Academic", image: "https://i.imgbox.com/Vj2gvMgO.jpeg"},
-  {name: "Fanfiction", image: "https://i.imgbox.com/K97zPBGy.jpeg"},
-  {name: "Spiritual", image: "https://i.imgbox.com/mZ79nNbG.png"},
-  {name: "Search Results", image: nil}
-])
+# Category.create!([
+#   {name: "Poetry", image: "https://i.imgbox.com/DsQP3SnJ.jpeg"},
+#   {name: "Fantasy", image: "https://i.imgbox.com/GU7n9JQm.jpeg"},
+#   {name: "Horror", image: "https://i.imgbox.com/bA8gM4Aw.jpg\n"},
+#   {name: "Humor", image: "https://i.imgbox.com/fPIqexsE.jpeg"},
+#   {name: "Literary Fiction", image: "https://i.imgbox.com/ejkGXRSl.jpg"},
+#   {name: "Science Fiction", image: "https://i.imgbox.com/Iy71DSpt.jpeg\n"},
+#   {name: "Romance", image: "https://i.imgbox.com/xkS7zeyi.jpg\n"},
+#   {name: "Children and YA", image: "https://i.imgbox.com/n49wqpcj.jpeg\n"},
+#   {name: "Scriptwriting", image: "https://i.imgbox.com/CqQrByg4.jpeg"},
+#   {name: "Nonfiction", image: "https://i.imgbox.com/vmuLzxQ2.jpeg"},
+#   {name: "Mystery", image: "https://i.imgbox.com/ogarDhmW.jpeg"},
+#   {name: "Academic", image: "https://i.imgbox.com/Vj2gvMgO.jpeg"},
+#   {name: "Fanfiction", image: "https://i.imgbox.com/K97zPBGy.jpeg"},
+#   {name: "Spiritual", image: "https://i.imgbox.com/mZ79nNbG.png"},
+#   {name: "Search Results", image: nil}
+# ])
